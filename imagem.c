d44d70c8221cf36cb2bcf2b55e4e0ae656b6b59a
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2024-05-08 11:09:03 -0300

    funcoes francinaldo

--- imagem.c
+++ imagem.c
@@ -5,13 +5,17 @@ typedef struct{
     int red, blue, green;
 }Pixel;
 
+
 typedef struct imagem
 {
     int altura, largura;
     Pixel *pixels;
 } Imagem;
 
-int main(){
+void printDimesoesImagens(Imagem *img){
+    printf("Altura: %d\nLargura: %d\n", img->altura, img->largura);
+}
 
-    return 0;
+void printPixel(int lin, int col, Imagem *img){
+    printf("%d, %d, %d", img->pixels[lin*img->largura+col].red, img->pixels[lin*img->largura+col].green, img->pixels[lin*img->largura+col].blue);
 }
--- imagem.h
+++ imagem.h
@@ -2,8 +2,8 @@ typedef struct pixel PixelRGB;
 
 typedef struct imagem Imagem;
 
-void printDimesoesImagens(Imagem *img);
-void printPixel(int lin, int col, Imagem *img);
+void printDimesoesImagens(Imagem *img); // Francinaldo
+void printPixel(int lin, int col, Imagem *img); // Francinaldo
 PixelRGB getPixel(int lin, int col, Imagem *img);
 PixelRGB setPixel(int lin, int col, Imagem *img);
 void printImagem(Imagem *img);
