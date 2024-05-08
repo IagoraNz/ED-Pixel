bcd51411c4ffc9c45a1fa30d97b263211fb9c383
JonasCGN <jonasbo66@gmail.com>
2024-05-08 10:54:52 -0300

    att, terminei?

--- imagem.c
+++ imagem.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 #include "imagem.h"
 
 typedef struct{
     int red, blue, green;
 }Pixel;
 
-typedef struct imagem
-{
+typedef struct{
     int altura, largura;
     Pixel *pixels;
-} Imagem;
+}Imagem;
 
+void printImagem(Imagem *img){
 
-int main(){
+    for(int i=0;i<img->altura;i++){
+        for(int j=0;j<img->largura;j++){
+            printf("%d,%d,%d", img->pixels[i * (img->largura) + j].red, img->pixels[i * (img->largura) + j].green,img->pixels[i * (img->largura) + j].blue);
+        }
+        printf("\n");
+    }
 
-    return 0;
 }
