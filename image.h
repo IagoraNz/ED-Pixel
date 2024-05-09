a3178dd4f18c53a9d503b5ab93c5b47cf2215446
JonasCGN <jonasbo66@gmail.com>
2024-05-09 10:12:06 -0300

    add

---
+++ image.h
@@ -0,0 +1,14 @@
+typedef struct pixel PixelRGB;
+typedef struct image Image;
+
+//nova
+typedef struct pixelgray Pixelgray;
+typedef struct image ImageGray;
+/*
+Novas funcoes
+- ler txt e converter em imagem -> Image
+- exportar imagem para txt -> txt salvo
+- Converter imagem RGB para nivel de cinza, isto é, com um único valor de pixel -> ImageGray
+- aplicar clusterizacao na imagem a partir de um arquivo txt com sementes, 
+	adtaptar a funcao da distancia euclidiana para considerar apenas os valores do pixel -> salvar txt com o resultado da clusterizacao
+*/
