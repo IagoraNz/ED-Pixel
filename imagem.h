400a569fc5fe7bbd4b362b8cc701c7150038fc90
JonasCGN <jonasbo66@gmail.com>
2024-05-09 10:27:41 -0300

    att

--- imagem.h
+++ imagem.h
@@ -4,6 +4,16 @@ typedef struct imagem Imagem;
 typedef struct pixelgray Pixelgray;
 typedef struct image ImageGray;
 
+/*
+Novas funcoes
+- ler txt e converter em imagem -> Image
+- exportar imagem para txt -> txt salvo
+- Converter imagem RGB para nivel de cinza, isto é, com um único valor de pixel -> ImageGray
+- aplicar clusterizacao na imagem a partir de um arquivo txt com sementes, 
+	adtaptar a funcao da distancia euclidiana para considerar apenas os valores do pixel -> salvar txt com o resultado da clusterizacao
+*/
+
+
 void printDimesoesImagens(Imagem *img); // Francinaldo
 void printPixel(int lin, int col, Imagem *img); // Francinaldo
 
@@ -14,3 +24,4 @@ void setPixel(int lin, int col, Imagem *img);
 
 //Mostrar os pixels da imagem, em forma de matriz
 void printImagem(Imagem *img);
+
